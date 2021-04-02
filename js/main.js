f42828272628dd02add513b5077d7f5661923a9f
Oriol Olivé Comadira <ori@MacBook-MacBook-Pro-de-Oriol.local>
2021-04-02 13:23:17 +0200

    first mods

---
+++ js/main.js
@@ -0,0 +1,48 @@
+
+var vm = new Vue({
+ el: '#vue-app',
+ data: {
+   articles:[],
+ },
+
+ data () {
+   return {
+     articles:[],
+   }
+ },
+mounted () {
+    this.loading = true;
+    self = this;
+    //
+    axios.get('articles.json')
+    .then(response => (this.articles = response.data)).
+    finally(function(){
+      self.loading=false;
+
+      console.log(self.data);
+
+    });
+
+},
+});
+
+var vm = new Vue({
+ el: '#vue-header',
+ data: {
+   isActiveBurger: false,
+   loadingBtn: false,
+   message: {
+     missatge: '',
+     status: '',
+   },
+ },
+
+ methods:{
+   // generals
+   toggleBurger: function(){
+       this.isActiveBurger = !this.isActiveBurger;
+       //console.log('toggle');
+   },
+ },
+
+});
